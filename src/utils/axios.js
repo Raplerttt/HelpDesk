0d7de315a6844024d9b9ed29bd41513a3db0a979
mac <mac@macs-MacBook-Pro.local>
2024-11-10 21:44:57 +0700

    axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,25 +1,30 @@
 import axios from "axios";
 
-// Membuat instance Axios dengan konfigurasi dasar
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // Ganti dengan baseURL backend Anda
+export default axios.create({
+  baseURL: "https://http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-// Menambahkan interceptor untuk menyertakan token di setiap permintaan
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = sessionStorage.getItem("token"); // Ambil token dari sessionStorage
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`; // Menyertakan token dalam header
+// const axiosInstance = axios.create({
+  axios.interceptors.request.use(
+    (config) => {
+      const token = sessionStorage.getItem("token"); // Ambil token dari sessionStorage
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`; // Menyertakan token dalam header
+      }
+      return config; // Mengembalikan konfigurasi yang telah diperbarui
+    },
+    (error) => {
+      return Promise.reject(error); // Menangani error jika ada
     }
-    return config; // Mengembalikan konfigurasi yang telah diperbarui
-  },
-  (error) => {
-    return Promise.reject(error); // Menangani error jika ada
-  }
-);
+  );
+//   baseURL: "http://localhost:5000/api", // Ganti dengan baseURL backend Anda
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-export default axiosInstance; // Ekspor instance Axios yang telah dikonfigurasi
+// // Menambahkan interceptor untuk menyertakan token di setiap permintaan
+
+// export default axiosInstance; // Ekspor instance Axios yang telah dikonfigurasi
