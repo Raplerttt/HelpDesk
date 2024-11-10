973e1eca17b17c2ff5cc8ceb4f02212d767f77fd
mac <mac@macs-MacBook-Pro.local>
2024-11-10 21:50:55 +0700

    module

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  
+  // Use findByText for asynchronous rendering (waiting for the element to appear)
+  const linkElement = await screen.findByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+
+
+// import { render, screen } from '@testing-library/react';
+// import { act } from 'react'; // Update this import
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
